35664beef8254a551ddd07b73ad838d55e1fc5a8
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-20 18:00:22 +0300

    Create day2-Project-Tip-Calculator.py

---
+++ day2-Project-Tip-Calculator.py
@@ -0,0 +1,9 @@
+# Project Tip Calculator
+# Instructions
+# Write a program that builts Tip-Calculator. 
+# The Tip-Calculator should welcomes the user.
+# The Tip-Calculator should prompt the user for:
+# 1) the total bill to be paid,
+# 2) the number of people the bill is to be split amongst.
+# 3) show the user the percentage range and ask them what percentage tip they would like to give 
+# The Tip-Calculator should show the user the amount of money each person should contribute.
