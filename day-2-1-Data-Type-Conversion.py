72e10ba848d8c5dfdab124038ccd0a6d8625b662
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-20 18:20:56 +0300

    Update day-2-1-Data-Type-Conversion.py

--- day-2-1-Data-Type-Conversion.py
+++ day-2-1-Data-Type-Conversion.py
@@ -1,3 +1,16 @@
+# Data Types
+# Instructions
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+# Example Input: 39
+# Example Output: 3 + 9 = 12
+
+# Hint
+# 1. Try to find out the data type of two_digit_number.
+# 2. Think about what you learnt about subscripting.
+# 3. Think about type conversion.
+
+# Source Code
+
 # 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
@@ -5,25 +18,17 @@ two_digit_number = input("Type a two digit number: ")
 ####################################
 #Write your code below this line 👇
 # find out the data type of the input received
+print(type(two_digit_number)) # the input received from the user will be a string
 
-print(type(two_digit_number)) #string
-
-#Convert the string data type to intergers
+# Converting the string data type to intergers
 first_digit = int(two_digit_number[0])
 last_digit = int(two_digit_number[1])
 
 # Find the data type, checks if conversion was successful
-print(type(first_digit)) #data type is interger
+print(type(first_digit)) # successful conversion of string data type to interger
 print(type(last_digit))
 
-# Print the sum
-print("The sum of the two-digit-number is:\n")
+# Prints the sum of the two-digit-number
+print("The sum of the two-digit-number is: ")
 sum = first_digit + last_digit
 print(sum)
-
-
-
-
-
-
-
